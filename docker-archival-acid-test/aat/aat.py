e2718ab8a15f208dc70b257742746c98e843e816
Andy Jackson <anj@anjackson.net>
2015-11-05 23:21:45 +0000

    Put archivalAcidTest into it's own container.

---
+++ docker-archival-acid-test/aat/aat.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, send_from_directory
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='/')
+
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>')
+def send_aat(path):
+    return send_from_directory('archivalAcidTest',path)
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0',debug=True)
