4566bcae5711bab6cf39d71e168922928406ec07
Andy Jackson <anj@anjackson.net>
2021-09-23 09:37:08 +0100

    Update Acid Test to simulate remote resources within the one container.

--- docker-archival-acid-test/aat/aat.py
+++ docker-archival-acid-test/aat/aat.py
@@ -3,6 +3,10 @@ from flask import Flask, request, send_from_directory
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='/')
 
+@app.route('/~mkelly/acid/<path:path>')
+def send_aat_remote(path):
+    return send_from_directory('archivalAcidTestRemote',path)
+
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path:path>')
 def send_aat(path):
