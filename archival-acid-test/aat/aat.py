074ca2ee8cb37a5e4888e4f6dcf37083a1a7d6e3
Andy Jackson <anj@anjackson.net>
2021-09-23 10:13:56 +0100

    Updated to serve files correctly.

--- archival-acid-test/aat/aat.py
+++ archival-acid-test/aat/aat.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, send_from_directory
 
-# set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='/')
+# Set up a simple Flask app
+app = Flask(__name__)
 
 @app.route('/~mkelly/acid/<path:path>')
 def send_aat_remote(path):
     return send_from_directory('archivalAcidTestRemote',path)
 
-@app.route('/', defaults={'path': 'index.html'})
+@app.route('/')
+def send_aat_index():
+    return send_from_directory('archivalAcidTest', 'index.html')
+
 @app.route('/<path:path>')
 def send_aat(path):
     return send_from_directory('archivalAcidTest',path)
